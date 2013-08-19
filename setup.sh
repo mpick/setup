1deb2198f5202809f5191e194e02e8871928a680
Miles Pickens <miles@vboxiMac>
2013-08-18 18:31:42 -0700

    Added Nodemon to the install list

--- setup.sh
+++ setup.sh
@@ -17,6 +17,11 @@ nvm use v0.10.12
 # http://jshint.com/
 npm install -g jshint
 
+# Install Nodemon for restarting the Node server at any time
+# nodemon [your app name]
+# rs to restart
+npm install -g nodemon
+
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
